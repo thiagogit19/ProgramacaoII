33d01bab730fedde53ee610ff2966509c11b65cc
†ђเลφ๏ җล√เэя <thiago_xavier.2002@hotmail.com>
2019-08-28 14:01:26 -0300

    vfvf

--- Lista16/disciplina/MainWindow.xaml.cs
+++ Lista16/disciplina/MainWindow.xaml.cs
@@ -28,26 +28,37 @@ namespace disciplina
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Historico h = new Historico(aluno.Text);
-            h.Inserir(new Disciplina());
+            h.Inserir(new Disciplina(disc.Text, sem.Text, int.Parse(media.Text)));
             // h.Inserir(new Disciplina("Design", 70));
             // h.Inserir(new Disciplina("Física", 95));
             list.ItemsSource = h.Listar(); // h.discs;
             txt.Text = h.IRA().ToString();
+
+            // Aprovação
+            bool ba;
+            if (check1.IsChecked == true) ba = true;
+            else ba = false;
+
         }
 
         class Disciplina
         {
-            private string nome;
+            private string nome, semestre;
             private int media;
-            public Disciplina(string nome, int media)
+            private bool aprovado;
+
+            public Disciplina(string nome, string sem, int media)
             {
                 this.nome = nome;
                 this.media = media;
+                semestre = sem;
             }
+
             public override string ToString()
             {
-                return nome;
+                return $"{nome} - {semestre}";   
             }
+
             public int GetMedia()
             {
                 return media;
@@ -59,20 +70,24 @@ namespace disciplina
             private string aluno;
             private Disciplina[] discs = new Disciplina[10];
             private int k;
+
             public Historico(string aluno)
             {
                 this.aluno = aluno;
             }
+
             public void Inserir(Disciplina d)
             {
                 if (k < 10) discs[k++] = d;
             }
+
             public Disciplina[] Listar()
             {
                 Disciplina[] novo = new Disciplina[k];
                 Array.Copy(discs, novo, k);
                 return novo;
             }
+
             public double IRA()
             {
                 if (k == 0) return 0;
