7353677744cb7956a3b872d088cd995621cc0ab1
†ђเลφ๏ җล√เэя <thiago_xavier.2002@hotmail.com>
2019-09-17 13:41:12 -0300

    Ex.01 concluído

---
+++ ex01 - contato/ex01 - contato/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex01___contato
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        contato x;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            x = new contato();
+            x.Nome = nome.Text;
+            x.Email = email.Text;
+            x.Nascimento = DateTime.Parse(nascimento.Text);
+            lista.Items.Add(x.ToString());
+        }
+
+
+    }
+}
---
+++ ex01 - contato/ex01 - contato/contato.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01___contato
+{
+    class contato
+    {
+        private string nome, email;
+        private DateTime nascimento;
+
+        public string Nome{
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public DateTime Nascimento
+        {
+            get { return nascimento; }
+            set { nascimento = value; }
+        }
+
+        public int Idade
+        {
+            get { return DateTime.Now.Year - nascimento.Year;  }
+        }
+
+        public override string ToString()
+        {
+            return $"{nome} - {email} - {nascimento} - {Idade}";
+        }
+    }
+}
