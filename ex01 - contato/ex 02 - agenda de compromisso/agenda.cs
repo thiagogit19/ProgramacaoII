875e16542cb5dcd126614941504712b1ad4c4c7e
†ђเลφ๏ җล√เэя <thiago_xavier.2002@hotmail.com>
2019-09-17 14:29:02 -0300

    confirmar 02

---
+++ ex01 - contato/ex 02 - agenda de compromisso/agenda.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_02___agenda_de_compromisso
+{
+    class agenda
+    {
+        private compromisso[] comps;
+        private int k;
+        private int quant;
+
+        public void Inserir{
+
+        }
+
+        public void Excluir{
+
+        }
+
+        public compromisso[] Listar()
+        {
+
+        }
+
+        public compromisso[] Pesquisar(int m, int a)
+        {
+
+        }
+
+
+
+        
+    }
+}
---
+++ ex01 - contato/ex 02 - agenda de compromisso/compromisso.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_02___agenda_de_compromisso
+{
+    class compromisso
+    {
+        private string assunto;
+        private string local;
+        private DateTime data;
+
+        public string Assunto {
+            get { return assunto;  }
+            set { assunto = value;  }
+        }
+
+        public string Local
+        {
+            get { return local; }
+            set { local = value; }
+        }
+
+        public DateTime Data {
+            get { return data;  }
+            set { data = value;  }
+        }
+
+        public override string ToString()
+        {
+            return $"{assunto} - {local} - {data}";
+        }
+    }
+}
