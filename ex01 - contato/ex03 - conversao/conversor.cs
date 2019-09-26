1d6ac5467fc61647e5e62ed5a3c4e9bb9c3b2a03
†ђเลφ๏ җล√เэя <thiago_xavier.2002@hotmail.com>
2019-09-26 14:54:41 -0300

    hyjyt

---
+++ ex01 - contato/ex03 - conversao/conversor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex03___conversao
+{
+    class conversor
+    {
+        public long Decimal
+        {
+
+        }
+
+        public string binario
+        {
+
+        }
+
+        public string hexa
+        {
+
+        }
+    }
+}
